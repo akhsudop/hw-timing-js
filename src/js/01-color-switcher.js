26fe3596ede1814f69a3786aa3e620470753d074
Alina <109147402+akhsudop@users.noreply.github.com>
2023-04-15 16:25:41 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+let timerId = null;
+stop.setAttribute('disabled', '');
 
+const getRandomHexColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const startSwitch = e => {
+  start.toggleAttribute('disabled');
+  stop.toggleAttribute('disabled');
+  document.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+start.addEventListener('click', startSwitch);
+
+const stopSwitch = e => {
+  clearInterval(timerId);
+  start.toggleAttribute('disabled');
+  stop.toggleAttribute('disabled');
+};
+stop.addEventListener('click', stopSwitch);
